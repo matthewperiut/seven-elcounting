4cd01a33b3c444b9770787d5aad7055ceb7de9a9
CarsonShattuck <therealneongalaxy@gmail.com>
2024-02-13 18:01:44 -0500

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,55 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const signup = async () => {
 
+  }
+  const login = async () => {
+      
+  }
+  const logout = async () => {
+      
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Seven-Elcounting</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div name="main">
+      <h1 id="header">Seven-Elcounting</h1>
+      <form action="">
+        <div name="login">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            placeholder="Username"
+          />
+        </div>
+        <div name="login">
+          <input
+            type="text"
+            name="password"
+            id="password"
+            required
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <a href="#">Forgot password?</a>
+        </div>
+        <button>Login</button>
+        <div>
+            <a href="#">Create an account!</a>
+        </div>
+      </form>
+    </div>
+  </body>
     </>
   )
 }
